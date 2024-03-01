8b9a1da83129b33baf4d7f9117fc9903e7c77dc9
DimamonDESKTOP <dimamon02@gmail.com>
2024-03-01 15:37:10 +0200

    done

--- dz05.03/Program.cs
+++ dz05.03/Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿using System;
+
+class Program
+{
+    static void DisplaySquare(int sideLength, char sym)
+    {
+        for (int i = 0; i < sideLength; i++)
+        {
+            for (int j = 0; j < sideLength; j++)
+            {
+                Console.Write(sym + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    static void Main()
+    {
+        DisplaySquare(5, '♥');
+    }
+}
