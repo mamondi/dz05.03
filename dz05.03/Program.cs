c2ddf56f69b6babd80cb4ab8c60ea820e0db1db8
DimamonDESKTOP <dimamon02@gmail.com>
2024-03-01 14:23:15 +0200

    done

--- dz05.03/Program.cs
+++ dz05.03/Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿using System;
+
+class Program
+{
+    static bool IsFibonacci(int num)
+    {
+        bool IsPerfectSquare(int x)
+        {
+            int sqrt = (int)Math.Sqrt(x);
+            return sqrt * sqrt == x;
+        }
+
+        return IsPerfectSquare(5 * num * num + 4) || IsPerfectSquare(5 * num * num - 4);
+    }
+
+    static void Main(string[] args)
+    {
+        int number = 8;
+        bool isFibonacci = IsFibonacci(number);
+        Console.WriteLine($"Число {number} є числом Фібоначчі: {isFibonacci}");
+    }
+}
