db4e4d9f1dd3e82d426af23b2d71d73231bd5d3a
DimamonDESKTOP <dimamon02@gmail.com>
2024-03-01 12:27:48 +0200

    done

--- dz05.03/Program.cs
+++ dz05.03/Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿using System;
+
+class Program
+{
+    static int MultiplyInRange(int start, int end)
+    {
+        int result = 1;
+        for (int i = start; i <= end; i++)
+        {
+            result *= i;
+        }
+        return result;
+    }
+
+    static void Main(string[] args)
+    {
+        //Тут вводити межі діапазону
+        int start = 1; int end = 5;
+
+        int product = MultiplyInRange(start, end);
+        Console.WriteLine($"Добуток чисел в діапазоні від {start} до {end} дорівнює {product}");
+    }
+}
